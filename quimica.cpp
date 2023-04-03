c4d1b9281790cda319c5710a633002acc420a14b
Gilson Junio Sales Andrade <83250268+GilsonJunio@users.noreply.github.com>
2023-04-03 09:31:01 -0400

    meu primeiro codigo em cpp que funcionou

---
+++ quimica.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main (){
+	string elemento_nome[2]={"Hidrogenio","Oxigenio"};
+	string elemento_sigla[2]={"H","O"};
+	int elemento_a[2]={1,16};
+	int elemento_z[2]={1,8};
+
+	cout<<"teste";
+	return 0;
+}
